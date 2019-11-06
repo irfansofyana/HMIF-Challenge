30623b777f3d51bd1ed82cc8e56b3f498952f36b
irfansofyana <irfansofyana0305@gmail.com>
2019-11-06 12:32:36 +0700

    add problems Josep-split-array

---
+++ Hmif-challenge-Josep-split-array/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[1003][55];
+long long prefiks[1005];
+int n, m;
+
+long long query(int a,int b){
+    return prefiks[b] - (a == 0 ? 0 : prefiks[a-1]);    
+}
+
+long long cari(int pos, int rem){
+    if (pos == n && rem == 0){
+        return 0;
+    }
+    if (pos == n || rem < 0){
+        return 1e18;
+    }
+    if (rem == 0 && pos != n){
+        return 1e18;
+    }
+    long long &ret = dp[pos][rem];
+    if (ret != -1) return ret;
+    for (int i = pos; i < n; ++i){
+        if (ret == -1){
+            ret = max(query(pos, i), cari(i+1, rem-1));
+        }else {
+            ret = min(ret, max(query(pos, i), cari(i+1, rem-1)));
+        }
+    }
+    return ret;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i){
+        long long x; cin >> x;
+        prefiks[i] = (i == 0 ? x : prefiks[i-1] + x);
+    }
+    memset(dp, -1, sizeof dp);
+    cout << cari(0, m) << '\n';
+    return 0;
+}
