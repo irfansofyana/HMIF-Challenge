dd9d85f352175718520459a1ded70eb2d7e473e8
irfansofyana <irfansofyana0305@gmail.com>
2019-11-17 21:32:43 +0700

    add problem permutasi

---
+++ Hmif-challenge-permutasi/spec.cpp
@@ -0,0 +1,43 @@
+#include <tcframe/spec.hpp>
+using namespace tcframe;
+using namespace std;
+
+const int MAXN = 100000;
+
+class ProblemSpec : public BaseProblemSpec{
+    protected:
+        int N, K;
+        vector<int> ans;
+
+    void InputFormat(){
+        LINE(N, K);
+    }
+
+    void OutputFormat(){
+        LINE(ans);
+    }
+
+    void GradingConfig(){
+        TimeLimit(1);
+        MemoryLimit(64);
+    }
+
+    void Constraints(){
+        CONS(1 <= N && N <= MAXN);
+        CONS(0 <= K && K < N);
+    }
+};
+
+class TestSpec : public BaseTestSpec<ProblemSpec>{
+    protected: 
+        void TestCases(){
+            CASE(N = 5, K = 3);
+            CASE(N = 7, K = 4);
+            for (int i = 0; i < 10; ++i){
+                CASE(N = rnd.nextInt(1, 2000), K = rnd.nextInt(0, N-1));
+            }
+            for (int i = 0; i < 15; ++i){
+                CASE(N = rnd.nextInt(2001, MAXN), K = rnd.nextInt(0, N-1));
+            }
+        }
+};
